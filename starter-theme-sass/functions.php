ef66b0ce2789a4c71973c14759aeec24a4141ea6
Doa Roys <doaelizabethroys@gmail.com>
2025-04-08 19:35:54 +1000

    Add code to remove sidebar

--- starter-theme-sass/functions.php
+++ starter-theme-sass/functions.php
@@ -119,20 +119,20 @@ add_action( 'after_setup_theme', 'starter_theme_sass_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-// function starter_theme_sass_widgets_init() {
-// 	register_sidebar(
-// 		array(
-// 			'name'          => esc_html__( 'Sidebar', 'starter-theme-sass' ),
-// 			'id'            => 'sidebar-1',
-// 			'description'   => esc_html__( 'Add widgets here.', 'starter-theme-sass' ),
-// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-// 			'after_widget'  => '</section>',
-// 			'before_title'  => '<h2 class="widget-title">',
-// 			'after_title'   => '</h2>',
-// 		)
-// 	);
-// }
-// add_action( 'widgets_init', 'starter_theme_sass_widgets_init' );
+function starter_theme_sass_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'starter-theme-sass' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'starter-theme-sass' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'starter_theme_sass_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -177,6 +177,6 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 function remove_sidebar_widget() {
-    unregister_sidebar( 'sidebar-1' );
+    unregister_sidebar( 'sidebar-1' ); 
 }
 add_action( 'widgets_init', 'remove_sidebar_widget', 11 );
